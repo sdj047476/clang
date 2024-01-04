0b1f036c96a662645f3a81023a551447f82e47aa
sdj047476 <155497581+sdj047476@users.noreply.github.com>
2024-01-04 10:54:01 +0900

    Add files via upload

---
+++ Day 02/scan.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+
+	//int price;
+	//printf("가격을 입력:");
+	//scanf_s("%d", &price);
+	//printf("대입하신 가격은 %d원 입니다.", price);
+
+	//int score_A, score_B;
+	//printf("A점수를 입력:");
+	//scanf_s("%d", &score_A);
+	//printf("B점수를 입력:");
+	//scanf_s("%d", &score_B);
+	//printf("두수의 합: %d", score_A + score_B);
+
+	printf("input:");
+	char a;
+	scanf_s("%c", &a);
+	printf("%c%c%c\n", a, a, a);
+	printf("%c %c\n", a, a);
+	printf("%c%c%c\n", a, a, a);
+
+
+
+	return 0;
+}
+
---
+++ Day 02/var_quiz.cpp
@@ -0,0 +1,43 @@
+//var_quiz
+
+#include<stdio.h>
+
+int main() {
+
+	//1. 2024 - (태어난 년도)
+	//printf("태어난 년도:");
+	//int BornYear;
+	//scanf_s("%d", &BornYear);
+	//printf("현재 만나이:%d", 2024 - BornYear);
+
+	//2. 세 개 숫자 입력, 평균
+	//int FirstNum, SecondNum, ThirdNum;
+	//printf("첫번째 수:");
+	//scanf_s("%d", &FirstNum);
+	//printf("두번째 수:");
+	//scanf_s("%d", &SecondNum);
+	//printf("세번째 수:");
+	//scanf_s("%d", &ThirdNum);
+	//printf("세수의 평균:%lf", (double)(FirstNum + SecondNum + ThirdNum) / 3);
+
+	//3. 섭씨 온도, 화씨로 변환 (F = Cx5.9 + 32)
+	int CentigradeScale;
+	printf("현재 섭씨 온도:");
+	scanf_s("%d", &CentigradeScale);
+	printf("현재 화씨 온도는 %lf입니다.", (double)CentigradeScale * 5.9 + 32);
+
+	//4. 한변의 길이를 입력, 정사각형 넓이
+
+
+	//5. 반지름 길이, 원의 넓이와 둘레 구하기
+
+
+
+
+
+	return 0;
+}
+
+
+
+
