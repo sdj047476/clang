1048085849134dce4b1763825e46d60b013d09d0
sdj047476 <155497581+sdj047476@users.noreply.github.com>
2024-01-23 10:48:07 +0900

    Add files via upload

---
+++ Day 15/linkedlist_final.cpp
@@ -0,0 +1,59 @@
+// linkedlist_final - 1
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node {
+	int data;
+	struct Node* next;
+}Node;
+
+Node* createNode() {
+	Node* newNode;
+	newNode = (Node*)malloc(sizeof(Node));
+	int n;
+	printf("노드 값 입력. ");
+	scanf("%d", &n);
+	newNode->data = n;
+	newNode->next = NULL;
+	return newNode;
+}
+
+void appendNode(Node** head) {
+	Node* target;
+	target = createNode();
+	if (*head == NULL) {
+		*head = target;
+		return;
+	}
+	else {
+		Node* searcher = *head;
+		while (searcher->next != NULL) {
+			searcher = searcher->next;
+		}
+		searcher->next = target;
+	}
+}
+
+void printNode(Node** head) {
+	Node* searcher = *head;
+	while (searcher->next != NULL) {
+		printf("%d -> ", searcher->data);
+		searcher = searcher->next;
+	}
+	printf("%d", searcher->data);
+}
+
+int main() {
+	Node* head = NULL;
+	int A;
+	printf("노드 반복 횟수. ");
+	scanf("%d", &A);
+
+	for (int i = 0; i < A; i++) {
+		appendNode(&head);
+	}
+
+	printNode(&head);
+
+	return 0;
+}
